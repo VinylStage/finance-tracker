5f73616cc5e430142217dc62deaed78c7369cf15
VinylStage <mcpe9869@gmail.com>
2026-08-03 08:22:54 +0900

    feat(db): 카드사 할부 정책 마스터 테이블 신설 (#266) (#292)

--- src/constants.js
+++ src/constants.js
@@ -9,4 +9,8 @@ const PAYMENT_STYLES = ['일시불', '할부', '리볼빙', '해당없음'];
 // 실패 시 자동 생성하는 값이라 포함한다.
 const MAJOR_TYPES = ['수입', '고정지출', '변동필수', '선택지출', '저축', '부채상환', '미분류'];
 
-module.exports = { PAYMENT_STYLES, MAJOR_TYPES };
+// card_installment_policies.policy_type 허용값의 정본(#266).
+// 부분무이자는 앞쪽 free_months 회차만 이자가 면제되고 그 뒤로는 유이자와 같다.
+const INSTALLMENT_POLICY_TYPES = ['무이자', '부분무이자', '유이자'];
+
+module.exports = { PAYMENT_STYLES, MAJOR_TYPES, INSTALLMENT_POLICY_TYPES };
